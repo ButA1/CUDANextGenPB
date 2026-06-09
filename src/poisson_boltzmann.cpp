db0772e44d57b3e905949845a7df4ada448fe79a
Alexander But <alexanderbut97@gmail.com>
2026-06-09 17:47:18 +0200

    forgot this one

--- src/poisson_boltzmann.cpp
+++ src/poisson_boltzmann.cpp
@@ -253,11 +253,6 @@ main (int argc, char **argv)
       std::cout << "\n== [ Starting numerical solution using LIS ] ==\n";
 
     pb.lis_compute_electric_potential (ray_cache);
-  } else if (pb.linear_solver_name == "cudss") {
-    if (rank == 0)
-      std::cout << "\n== [ Starting numerical solution using cuDSS ] ==\n";
-
-    pb.cudss_compute_electric_potential (ray_cache);
   } else if (pb.linear_solver_name == "amgx") {
     if (rank == 0)
       std::cout << "\n== [ Starting numerical solution using AMGX ] ==\n";
